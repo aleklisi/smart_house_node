684741667906047656face01544efe3422e1c687
Aleksander Lisiecki <sebaczechu123@gmail.com>
2019-02-10 22:40:01 +0100

    fixup! Extract sensor config params to macros

---
+++ hrl/sensor_params.hrl
@@ -0,0 +1,23 @@
+%% consts
+-define(SAVE_MEASUREMENT_MESSAGE, save_measurement).
+
+-define(DEFAULT_REPEATE_TIME, 5000).
+
+%% state elements
+
+-define(MEASUREMENT_NAME, m_name).
+
+-define(REPEAT_AFTER, repeat_after).
+
+-define(MEASUREMENT_FUN, measurement_function).
+
+-define(MEASUREMENT_FUN_ARGS,
+	measurement_function_arguments).
+
+-define(TERMONATE_SENSOR_FUN, terminate_fun).
+
+-define(INIT_SENSOR_FUN, init_fun).
+
+-define(TERMONATE_SENSOR_FUN_ARGS, terminate_fun_args).
+
+-define(INIT_SENSOR_FUN_ARGS, init_fun_args).
