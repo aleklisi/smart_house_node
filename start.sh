e7b434a63cfcbc558782dea409c4aa0d6b1a1615
Aleksander Lisiecki <sebaczechu123@gmail.com>
2019-09-09 21:23:32 +0200

    Extend start.sh for eth0 network interface

--- start.sh
+++ start.sh
@@ -6,15 +6,18 @@
 export PATH=/home/pi/.cache/rebar3/bin:$PATH
 export PATH=/bin/:/usr/bin:$PATH
 
-export IP_ADDRESS=$(ip addr show wlan0 \
+export IP_ADDRESS=$((ifconfig wlan0 || ifconfig eth0) \
     | grep -oP 'inet ([\d|\.]*)' \
     | grep -oP '([\d|\.]*)')
 
+echo "IP is $IP_ADDRESS"
 
 export NODE_NAME=$(cat /home/pi/Documents/Erlang/smart_house_node/priv/sys.config \
     | grep -oP 'prefix, "([a-zA-Z1-9]*)"' \
     | grep -oP '"([a-zA-Z1-9]*)"' \
     | grep -oP '([a-zA-Z1-9]*)')
+
+echo "NODE_NAME is $NODE_NAME"
 
 export COOKIE="house"
 
