ad3a5bdd417ff510c027001cfc487ac84bb693c6
Aleksander Lisiecki <sebaczechu123@gmail.com>
2019-12-26 16:56:03 +0100

    Add http reporter

---
+++ src/consumers/reporters/http_call_reporter.erl
@@ -0,0 +1,37 @@
+-module(http_call_reporter).
+
+-behavior(reporter_behaviour).
+
+-export([
+   child_spec/1,
+    init/1,
+    handle_info/2]).
+
+child_spec(Config) ->
+    #{
+        name := Name
+    } = Config,
+    #{
+        id => Name,
+        start =>
+            {reporter, start_link, [Config]},
+        restart => permanent,
+        shutdown => brutal_kill,
+        type => worker,
+        modules => [?MODULE, reporter]
+    }.
+
+init([Url]) ->
+    inets:start(),
+    Url.
+
+handle_info({measurements, Info}, State = State = #{init_result := Url}) ->
+    MapInfo = maps:from_list(Info),
+    Body = jiffy:encode(MapInfo),
+    Headers = [],
+    ContentType = "application/json",
+    Request = {Url, Headers, ContentType, Body},
+    R = httpc:request(post, Request, [], []),
+    logger:error("~p", [Body]),
+    logger:error("~p", [R]),
+    State.
